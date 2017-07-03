2d175d371103caa3d42df7178eee1e1b96e08de1
songluyi <slysly759@gmail.com>
2017-07-03 13:38:41 +0800

    lovely I find pycharm fast key：alt+alt+l to polish my code.:relaxed:

--- get2db.py
+++ get2db.py
@@ -27,14 +27,18 @@ from colorama import init, Fore, Back, Style
 import re
 # 改为sqllite3
 import logging
+
 logging.basicConfig(level=logging.INFO)
+
+
 class get2db(object):
     # 调用一次 传递一个游标
     def __init__(self):
         # 这个我想通过爬虫来判断QQ的男女 这个样通过emgon的外部库
         self.boy_name = '一只特立独行的猪'
-        self.girl_name= '一颗被拱了的白菜'
+        self.girl_name = '一颗被拱了的白菜'
         return
+
     def connect_db(self):
         # 没发现这个用dict 可以传递现在用函数传递游标也行吧
         import sqlite3
@@ -48,10 +52,11 @@ class get2db(object):
         );
         ''')
         return conn
+
     def get_path(self):
         import os
         current_path = os.path.abspath(os.path.join(os.path.dirname('get2db.py'), os.path.pardir))
-        new_path = current_path + 'lovetime'+'\\' + 'msg'+'\\'
+        new_path = current_path + 'lovetime' + '\\' + 'msg' + '\\'
         FileList = []
         rootdir = new_path
         for root, subFolders, files in os.walk(rootdir):
@@ -60,49 +65,50 @@ class get2db(object):
             #     subFolders.remove('done')
             # 查找txt 聊天文件
             for f in files:
-                if f.find('txt') != -1 :
+                if f.find('txt') != -1:
                     FileList.append(os.path.join(root, f))
 
         for item in FileList:
             print(Fore.WHITE + '检测到您目录下有如下txt聊天文件 请确认是不是你要进行检测')
             print(item)
         return FileList
+
     def check_format(self, path_dict):
         for file in path_dict:
-            file_count=0
+            file_count = 0
             with open(file, 'r', encoding="utf8") as check_file:
-                count=0
-                error_tag=0
+                count = 0
+                error_tag = 0
                 for line in check_file:
-                    count=count+1
-                    if count==4:
-                        if '消息分组' in line :
+                    count = count + 1
+                    if count == 4:
+                        if '消息分组' in line:
                             print(line)
                         else:
-                            error_tag+=1
-                    if count==6:
+                            error_tag += 1
+                    if count == 6:
                         if '消息对象' in line:
                             print(line)
-                            self.girl_name=line[9:]
+                            self.girl_name = line[9:]
                         else:
-                            error_tag+=1
-            if error_tag>0:
-                print(Fore.RED +'该文本不符合导入要求，已经从列表中删除')
+                            error_tag += 1
+            if error_tag > 0:
+                print(Fore.RED + '该文本不符合导入要求，已经从列表中删除')
                 del path_dict[file_count]
             else:
-                print(Fore.GREEN +'检测完成 下一步生成数据库文件')
-            file_count+=1
+                print(Fore.GREEN + '检测完成 下一步生成数据库文件')
+            file_count += 1
         return path_dict
 
     def check_title(self, string):
-        result=re.findall('\d{4}-\d{2}-\d{2} \d{1,2}:\d{2}:\d{2}', string)
+        result = re.findall('\d{4}-\d{2}-\d{2} \d{1,2}:\d{2}:\d{2}', string)
         if result:
             return True
         else:
             return False
 
-    def get_content(self,legal_path):
-        data=[]
+    def get_content(self, legal_path):
+        data = []
         for file in legal_path:
             with open(file, 'r', encoding='utf8') as qq_msg:
                 db_content = {}
@@ -113,33 +119,34 @@ class get2db(object):
                     if get2db().check_title(line):
                         # 如果存在上一行信息封装好 那么本次运行就插入到数据库 或者自己再做一个字典
                         if db_content:
-                            change_formate=(db_content['time'], db_content['content'], db_content['user'])
+                            change_formate = (db_content['time'], db_content['content'], db_content['user'])
                             data.append(change_formate)
-                            msg_content=[]
-                            db_content={}
-                        msg_time=re.findall('\d{4}-\d{2}-\d{2} \d{1,2}:\d{2}:\d{2}',line)
-                        msg_time=msg_time[0]
+                            msg_content = []
+                            db_content = {}
+                        msg_time = re.findall('\d{4}-\d{2}-\d{2} \d{1,2}:\d{2}:\d{2}', line)
+                        msg_time = msg_time[0]
                         # 一般来讲 QQ的用户名20个字符就足够了
-                        msg_user=re.findall('[\u4e00-\u9fa5]{1,20}',line)
+                        msg_user = re.findall('[\u4e00-\u9fa5]{1,20}', line)
                         if msg_user:
-                            msg_user=msg_user[0]
+                            msg_user = msg_user[0]
                         else:
-                            msg_user='用户名未查询到'
-                        db_content['time']=msg_time
-                        db_content['user']=msg_user
+                            msg_user = '用户名未查询到'
+                        db_content['time'] = msg_time
+                        db_content['user'] = msg_user
                     else:
                         msg_content.append(line)
-                        qq_content=''.join(msg_content)
-                        hh_content=qq_content.replace('\n', '')
-                        db_content['content']=hh_content
+                        qq_content = ''.join(msg_content)
+                        hh_content = qq_content.replace('\n', '')
+                        db_content['content'] = hh_content
         return data
-    def insert_db(self,data):
+
+    def insert_db(self, data):
         db = get2db().connect_db()
         cursor = db.cursor()
-        insert_sql="INSERT INTO msg(qq_time,qq_msg,qq_user) VALUES (?, ?, ?)"
-        select_sql="SELECT * FROM msg"
+        insert_sql = "INSERT INTO msg(qq_time,qq_msg,qq_user) VALUES (?, ?, ?)"
+        select_sql = "SELECT * FROM msg"
         cursor.execute(select_sql)
-        check_result=cursor.fetchall()
+        check_result = cursor.fetchall()
         # 如果数据库为空才导入，不为空则不导入
         if check_result:
             print(Fore.YELLOW + '数据库中已经存在了需要检测的聊天记录,本次不会导入！')
@@ -149,9 +156,8 @@ class get2db(object):
         db.commit()
 
 
-
-if __name__=="__main__":
-    msg=get2db()
-    my_path=msg.get_path()
-    my_content=msg.get_content(my_path)
+if __name__ == "__main__":
+    msg = get2db()
+    my_path = msg.get_path()
+    my_content = msg.get_content(my_path)
     msg.insert_db(my_content)
--- moniter.py
+++ moniter.py
@@ -35,84 +35,92 @@ from jieba import analyse
 # 效率的事情稍 后来转么进行实现 特别是规范化的问题
 import jieba
 import logging
+
 logging.basicConfig(level=logging.INFO)
+
+
 def log(msg):
     logging.info(str(msg))
-from get2db import get2db
-class moniter_platform(object):
 
+
+from get2db import get2db
+
+
+class moniter_platform(object):
     def __init__(self):
-        self.db_result=self.get_db_reslt()
-        self.first_strike_up=''
-        self.sec_strike_up=''
-        self.history_name=[]
-        self.chat_his=[]# 聊天历史年份
-        self.year_time=2# 默认时长两年
-        self.time_gap=[]
-        self.sql='select * from msg'
-        self.count_word={}
+        self.db_result = self.get_db_reslt()
+        self.first_strike_up = ''
+        self.sec_strike_up = ''
+        self.history_name = []
+        self.chat_his = []  # 聊天历史年份
+        self.year_time = 2  # 默认时长两年
+        self.time_gap = []
+        self.sql = 'select * from msg'
+        self.count_word = {}
 
     def get_db_reslt(self):
-        sql='select * from msg'
-        db=get2db().connect_db()
+        sql = 'select * from msg'
+        db = get2db().connect_db()
         cursor = db.cursor()
         cursor.execute(sql)
         check_result = cursor.fetchall()
         return check_result
 
     def visual_time(self):
-        check_result=self.get_field()
-        index_result=dict()
-        hour_result=dict()
+        check_result = self.get_field()
+        index_result = dict()
+        hour_result = dict()
         for i in check_result:
             # 开始桶排序 取前10字节 的日期
-            index_day=str(i)[0:10]
-            index_hour=str(i)[11:13]
-            count=index_result.get(index_day,0)
-            hour_count=hour_result.get(index_hour,0)
-            if int(count)>0:
-                middle_num=index_result[index_day]+1
-                index_result[index_day] =middle_num
+            index_day = str(i)[0:10]
+            index_hour = str(i)[11:13]
+            count = index_result.get(index_day, 0)
+            hour_count = hour_result.get(index_hour, 0)
+            if int(count) > 0:
+                middle_num = index_result[index_day] + 1
+                index_result[index_day] = middle_num
             else:
                 index_result[index_day] = 1
-            if int(hour_count)>0:
-                middle_hour=hour_result[index_hour]+1
-                hour_result[index_hour]=middle_hour
+            if int(hour_count) > 0:
+                middle_hour = hour_result[index_hour] + 1
+                hour_result[index_hour] = middle_hour
             else:
-                hour_result[index_hour]=1
-        day_list=sorted(self.dict2list(index_result), key=lambda x:x[1], reverse=True)
-        hour_list=sorted(self.dict2list(hour_result), key=lambda x:x[1], reverse=True)
+                hour_result[index_hour] = 1
+        day_list = sorted(self.dict2list(index_result), key=lambda x: x[1], reverse=True)
+        hour_list = sorted(self.dict2list(hour_result), key=lambda x: x[1], reverse=True)
 
-        return [day_list,hour_list]
+        return [day_list, hour_list]
 
-    def dict2list(self,dic):
+    def dict2list(self, dic):
         ''' 将字典转化为列表 '''
         keys = dic.keys()
         vals = dic.values()
         lst = [(key, val) for key, val in zip(keys, vals)]
         return lst
-    def turn_tuplelist(self,dic1,dic2):
-        new_dict=[]
+
+    def turn_tuplelist(self, dic1, dic2):
+        new_dict = []
         for i in range(0, len(dic1)):
-            new_dict.append((dic1[i],dic2[i]))
+            new_dict.append((dic1[i], dic2[i]))
         return new_dict
+
     # 获取数据库中字段，默认为获取时间
     # 仅接受 一个condition
     def get_field(self, num=3, time_limit=None, *condition):
         back_result = []
-        sql_condition=condition
+        sql_condition = condition
         # 存在condition where 语句 就重新执行sql 否则就用统一的
         if sql_condition:
-            sql_plus=' where '+str(sql_condition[0])+'='+"'"+str(sql_condition[1])+"'"
+            sql_plus = ' where ' + str(sql_condition[0]) + '=' + "'" + str(sql_condition[1]) + "'"
             all_sql = self.sql + sql_plus
-            db=get2db().connect_db()
+            db = get2db().connect_db()
             cursor = db.cursor()
             log(all_sql)
             cursor.execute(all_sql)
             check_result = cursor.fetchall()
         else:
-            sql_plus=''
-            check_result=self.db_result
+            sql_plus = ''
+            check_result = self.db_result
 
         # 如果db_result为空 就到数据区中取值 否则就用平时已经存储过的值
         # if not self.db_result:
@@ -126,11 +134,11 @@ class moniter_platform(object):
             if not time_limit:
                 back_result.append(i[num])
             else:
-                tail_time=datetime.datetime.strptime(time_limit[0],'%Y-%m-%d')
-                header_time=datetime.datetime.strptime(time_limit[1],'%Y-%m-%d')
-                compare_time=datetime.datetime.strptime(i[3],'%Y-%m-%d %H:%M:%S')
+                tail_time = datetime.datetime.strptime(time_limit[0], '%Y-%m-%d')
+                header_time = datetime.datetime.strptime(time_limit[1], '%Y-%m-%d')
+                compare_time = datetime.datetime.strptime(i[3], '%Y-%m-%d %H:%M:%S')
                 if compare_time <= header_time and compare_time >= tail_time:
-                     back_result.append(i[num])
+                    back_result.append(i[num])
         # else:
         #     for i in self.db_result:
         #         if not time_limit:
@@ -146,24 +154,24 @@ class moniter_platform(object):
 
     # 获取聊天的年份 方便对回复速率进行年份间的评估
     def get_chat_his(self):
-        time_data=self.get_field(3)
-        chat_his=[]
-        tail_flag=header_flag=False
-        count=0
+        time_data = self.get_field(3)
+        chat_his = []
+        tail_flag = header_flag = False
+        count = 0
         for i in time_data:
-            count+=1
-            if count==1 and int(str(i)[5:7])<7:
+            count += 1
+            if count == 1 and int(str(i)[5:7]) < 7:
                 print(int(str(i)[5:7]))
-                tail_flag=True
-            if count==len(time_data) and int(str(i)[5:7])>5:
-                header_flag=True
+                tail_flag = True
+            if count == len(time_data) and int(str(i)[5:7]) > 5:
+                header_flag = True
             if str(i)[0:4] not in chat_his:
                 chat_his.append(str(i)[0:4])
         # 第一个布尔类型表示最开始年份是否有半年之久
         # 第二个布尔类型表示后续年份超过六个月
         chat_his.append(tail_flag)
         chat_his.append(header_flag)
-        self.chat_his=chat_his
+        self.chat_his = chat_his
         log(chat_his)
         return chat_his
 
@@ -172,136 +180,139 @@ class moniter_platform(object):
         self.first_strike_up = reply_data[0][0]
         return self.first_strike_up
 
-
-    def reply_rate(self,interval_time=None):
-        reply_data=self.turn_tuplelist(self.get_field(2,interval_time),self.get_field(3,interval_time))
+    def reply_rate(self, interval_time=None):
+        reply_data = self.turn_tuplelist(self.get_field(2, interval_time), self.get_field(3, interval_time))
         # 第一次打招呼的人
         # print(reply_data)
         # first_strike_up=reply_data[0][0]
         # self.first_strike_up=first_strike_up
-        first_strike_up=self.get_first_strike()
-        first_gap_list=[]
-        sec_gap_list=[]
+        first_strike_up = self.get_first_strike()
+        first_gap_list = []
+        sec_gap_list = []
 
-        for i in range(0,len(reply_data)-1):
+        for i in range(0, len(reply_data) - 1):
             # 如果两条中的后一条不是最先打招呼的那人发的
-            if first_strike_up != str(reply_data[i+1][0]) :
-                self.sec_strike_up=str(reply_data[i+1][0])
+            if first_strike_up != str(reply_data[i + 1][0]):
+                self.sec_strike_up = str(reply_data[i + 1][0])
                 # 对 研究记录对象的 曾用名 进行统计
                 # print(first_strike_up)
-                filter_name=str(reply_data[i+1][0]).replace('系统消息','').replace('用户名未查询到','')
-                if filter_name not in self.history_name and filter_name !='':
-                    self.history_name.append(str(reply_data[i+1][0]))
+                filter_name = str(reply_data[i + 1][0]).replace('系统消息', '').replace('用户名未查询到', '')
+                if filter_name not in self.history_name and filter_name != '':
+                    self.history_name.append(str(reply_data[i + 1][0]))
                 # 由于改用sqllite 导致这里时间格式需要再脚本计算修改
-                gap2_time=datetime.datetime.strptime(reply_data[i+1][1],'%Y-%m-%d %H:%M:%S')
-                gap1_time=datetime.datetime.strptime(reply_data[i][1],'%Y-%m-%d %H:%M:%S')
-                first_gap_time=gap2_time - gap1_time
+                gap2_time = datetime.datetime.strptime(reply_data[i + 1][1], '%Y-%m-%d %H:%M:%S')
+                gap1_time = datetime.datetime.strptime(reply_data[i][1], '%Y-%m-%d %H:%M:%S')
+                first_gap_time = gap2_time - gap1_time
                 first_gap_list.append(first_gap_time)
-            elif first_strike_up==str(reply_data[i+1][0]):
-                gap2_time=datetime.datetime.strptime(reply_data[i+1][1],'%Y-%m-%d %H:%M:%S')
-                gap1_time=datetime.datetime.strptime(reply_data[i][1],'%Y-%m-%d %H:%M:%S')
-                sec_gap_time= gap2_time - gap1_time
+            elif first_strike_up == str(reply_data[i + 1][0]):
+                gap2_time = datetime.datetime.strptime(reply_data[i + 1][1], '%Y-%m-%d %H:%M:%S')
+                gap1_time = datetime.datetime.strptime(reply_data[i][1], '%Y-%m-%d %H:%M:%S')
+                sec_gap_time = gap2_time - gap1_time
 
                 sec_gap_list.append(sec_gap_time)
         # 这个数据格式还是满奇特的
-        first_sum_time=datetime.timedelta(0, 0)
-        sec_sum_time=datetime.timedelta(0, 0)
+        first_sum_time = datetime.timedelta(0, 0)
+        sec_sum_time = datetime.timedelta(0, 0)
         for i in first_gap_list:
-            first_sum_time=first_sum_time+i
+            first_sum_time = first_sum_time + i
         for j in sec_gap_list:
-            sec_sum_time=sec_sum_time+j
-        first_avg_time=first_sum_time/len(first_gap_list)
-        sec_avg_time=sec_sum_time/len(sec_gap_list)
+            sec_sum_time = sec_sum_time + j
+        first_avg_time = first_sum_time / len(first_gap_list)
+        sec_avg_time = sec_sum_time / len(sec_gap_list)
 
-        if int(interval_time[1][:4])-int(interval_time[0][:4])==1:
-            time_axis=interval_time[0][:4]+'下半年'
-        elif int(interval_time[1][:4])-int(interval_time[0][:4])==0:
-            time_axis=interval_time[0][:4]+'上半年'
+        if int(interval_time[1][:4]) - int(interval_time[0][:4]) == 1:
+            time_axis = interval_time[0][:4] + '下半年'
+        elif int(interval_time[1][:4]) - int(interval_time[0][:4]) == 0:
+            time_axis = interval_time[0][:4] + '上半年'
         else:
-            time_axis='Wrong'
+            time_axis = 'Wrong'
         print(self.history_name)
         print(Fore.GREEN + '平均回复' + '【' + self.first_strike_up + '】' + '的时间是：' + str(first_avg_time))
         print(Fore.GREEN + '平均回复' + '【' + self.sec_strike_up + '】' + '的时间是：' + str(sec_avg_time))
         return [time_axis, first_strike_up, sec_avg_time]
+
     def get_time_gap(self):
         if not self.chat_his:
             self.get_chat_his()
-        self.year_time=len(self.chat_his[:-2])
+        self.year_time = len(self.chat_his[:-2])
 
         for year in self.chat_his[:-2]:
-            self.time_gap.append(str(year)+'-01-01')
-            self.time_gap.append(str(year)+'-06-01')
+            self.time_gap.append(str(year) + '-01-01')
+            self.time_gap.append(str(year) + '-06-01')
         # 若存在最初日期大于六月份
         log(self.chat_his)
         if not self.chat_his[-2]:
-            self.time_gap=self.time_gap[1:]
+            self.time_gap = self.time_gap[1:]
         if not self.chat_his[-1]:
-            self.time_gap=self.time_gap[:-1]
+            self.time_gap = self.time_gap[:-1]
 
-        log('your time_gap %s'%self.time_gap)
+        log('your time_gap %s' % self.time_gap)
         return self.time_gap
-    def get_reply_fluency(self,time_gap=None):
-        appear_name_list=self.get_field(2,time_gap)
-        fluency_table=Counter(appear_name_list)
-        first_frequency=fluency_table[self.first_strike_up]/len(appear_name_list)
-        sec_frequency=1-first_frequency
-        first_ratio_reply=first_frequency/sec_frequency
-        sec_ratio_reply=sec_frequency/first_frequency
-        print(self.first_strike_up+"回复频率为 1: "+"%.2f" %first_ratio_reply)
+
+    def get_reply_fluency(self, time_gap=None):
+        appear_name_list = self.get_field(2, time_gap)
+        fluency_table = Counter(appear_name_list)
+        first_frequency = fluency_table[self.first_strike_up] / len(appear_name_list)
+        sec_frequency = 1 - first_frequency
+        first_ratio_reply = first_frequency / sec_frequency
+        sec_ratio_reply = sec_frequency / first_frequency
+        print(self.first_strike_up + "回复频率为 1: " + "%.2f" % first_ratio_reply)
         print(self.sec_strike_up + "回复频率为 1: " + "%.2f" % sec_ratio_reply)
-        return (first_frequency, 1-first_frequency)
-    def get_content_ratio(self,time_gap=None):
-        first_content_list=''.join(self.get_field(1,time_gap,'qq_user',self.first_strike_up))
-        first_content_length=len(first_content_list)
-        sec_content_list=''.join(self.get_field(1,time_gap,'qq_user',self.sec_strike_up))
-        sec_content_length=len(sec_content_list)
-        first_ratio_content=first_content_length/sec_content_length
-        sec_ratio_content=sec_content_length/first_content_length
-        print(self.first_strike_up+"内容回复比率为 1: "+"%.2f" %first_ratio_content)
+        return (first_frequency, 1 - first_frequency)
+
+    def get_content_ratio(self, time_gap=None):
+        first_content_list = ''.join(self.get_field(1, time_gap, 'qq_user', self.first_strike_up))
+        first_content_length = len(first_content_list)
+        sec_content_list = ''.join(self.get_field(1, time_gap, 'qq_user', self.sec_strike_up))
+        sec_content_length = len(sec_content_list)
+        first_ratio_content = first_content_length / sec_content_length
+        sec_ratio_content = sec_content_length / first_content_length
+        print(self.first_strike_up + "内容回复比率为 1: " + "%.2f" % first_ratio_content)
         print(self.sec_strike_up + "内容回复比率为 1: " + "%.2f" % sec_ratio_content)
 
     def jieba_count_word(self, time_gap=None):
         jieba.set_dictionary('foobar.txt')
         # 有些聊天词语 字典加了也不给划分 因此我在这里强制一下
-        jieba.suggest_freq(('会从','[表情]'))
-        msg_list=self.get_field(1, time_gap)
-        count_gap_word={}
+        jieba.suggest_freq(('会从', '[表情]'))
+        msg_list = self.get_field(1, time_gap)
+        count_gap_word = {}
         for single_msg in msg_list:
-            cut_sentence=jieba.cut(single_msg)
+            cut_sentence = jieba.cut(single_msg)
             for word in cut_sentence:
-                if not count_gap_word.get(word,None):
-                    count_gap_word.setdefault(word,1)
-                if not self.count_word.get(word,None):
-                    self.count_word.setdefault(word,1)
+                if not count_gap_word.get(word, None):
+                    count_gap_word.setdefault(word, 1)
+                if not self.count_word.get(word, None):
+                    self.count_word.setdefault(word, 1)
                 else:
-                    plus_one=count_gap_word[word]+1
-                    fuck_one=self.count_word[word]+1
-                    count_gap_word[word]=plus_one
-                    self.count_word[word]=fuck_one
+                    plus_one = count_gap_word[word] + 1
+                    fuck_one = self.count_word[word] + 1
+                    count_gap_word[word] = plus_one
+                    self.count_word[word] = fuck_one
 
-            # print(count_gap_word)
-        jieba_count=sorted(self.dict2list(count_gap_word),key=lambda x:x[1],reverse=True)
+                    # print(count_gap_word)
+        jieba_count = sorted(self.dict2list(count_gap_word), key=lambda x: x[1], reverse=True)
         # print(jieba_count)
         return jieba_count
 
 
 # 这些复杂的函数 到时还是写一个unittest
-moniter=moniter_platform()
-count=0
-moniter.get_time_gap()
-print(moniter.time_gap[:-1])
-for gap in moniter.time_gap[:-1]:
-    print(moniter.time_gap[count + 1])
-    small_gap=[moniter.time_gap[count], moniter.time_gap[count+1]]
-    moniter.reply_rate(small_gap)
-    moniter.get_reply_fluency(small_gap)
-    moniter.get_content_ratio(small_gap)
-    moniter.jieba_count_word(small_gap)
-    count += 1
-print(sorted(moniter.dict2list(moniter.count_word),key=lambda x:x[1],reverse=True))
-file_path=get2db().get_path()
-content = ''.join(moniter.get_field(1))
-tags = jieba.analyse.extract_tags(content,40)
-print(",".join(tags))
-# s=moniter.get_field(3,['2014-06-01', '2015-01-01'],'qq_user','名一')
-# print(s)
+if __name__ == "__main__":
+    moniter = moniter_platform()
+    count = 0
+    moniter.get_time_gap()
+    print(moniter.time_gap[:-1])
+    for gap in moniter.time_gap[:-1]:
+        print(moniter.time_gap[count + 1])
+        small_gap = [moniter.time_gap[count], moniter.time_gap[count + 1]]
+        moniter.reply_rate(small_gap)
+        moniter.get_reply_fluency(small_gap)
+        moniter.get_content_ratio(small_gap)
+        moniter.jieba_count_word(small_gap)
+        count += 1
+    print(sorted(moniter.dict2list(moniter.count_word), key=lambda x: x[1], reverse=True))
+    file_path = get2db().get_path()
+    content = ''.join(moniter.get_field(1))
+    tags = jieba.analyse.extract_tags(content, 40)
+    print(",".join(tags))
+    # s=moniter.get_field(3,['2014-06-01', '2015-01-01'],'qq_user','名一')
+    # print(s)
